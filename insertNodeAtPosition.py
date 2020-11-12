1e9fc0c152a94edd5aa7dcd3b8d5c043fa212bb6
Lukas Rimkus <lukasrimkuslg@gmail.com>
2020-11-12 16:16:12 +0100

    Update insertNodeAtPosition.py

--- insertNodeAtPosition.py
+++ insertNodeAtPosition.py
@@ -29,3 +29,17 @@ def insertNodeAtHead(head, data):
     if head:
         node.next = head
     return node
+
+def deleteNode(head, position):
+    current = head
+    # next will point to None if there is 
+    # not another item in the list
+    if position == 0:
+        head = head.next
+    else:
+    	# iterate to the right node
+        for i in range(position-1):
+            current = current.next
+        # and alter the next pointer
+        current.next = current.next.next
+    return head
